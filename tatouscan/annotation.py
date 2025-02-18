5240fc414c9ef2049774dc5c86a6b7d6fb544fe2
JeanMainguy <jean.mainguy@outlook.fr>
2025-02-18 11:09:27 +0100

    add logging info

--- tatouscan/annotation.py
+++ tatouscan/annotation.py
@@ -1,5 +1,8 @@
 from typing import List
 import pyhmmer.easel
+import logging
+from rich.progress import track
+
 
 from pyhmmer.plan7 import HMM
 from collections import defaultdict
@@ -8,6 +11,7 @@ from pathlib import Path
 
 from tatouscan.models import TaHit
 
+logger = logging.getLogger(__name__)
 
 def find_ta_hits(faa_file: Path, hmm_db: Path, e_value_threshold: float = 0.01):
     """
@@ -19,27 +23,40 @@ def find_ta_hits(faa_file: Path, hmm_db: Path, e_value_threshold: float = 0.01):
 
     :returns: A dictionary with protein ids as keys and a list of TaHit objects as values.
     """
-    proteins = None
-    with pyhmmer.easel.SequenceFile(faa_file, digital=True) as seqs_file:
-        proteins = seqs_file.read_block()
-
-    if proteins is None:
-        raise ValueError(f"No sequences found in {faa_file}")
-
+    logger.info(f"Loading HMM profiles from database: '{hmm_db}'")
     hmms: List[HMM] = []
     with pyhmmer.plan7.HMMFile(hmm_db) as hmm_file:
         for hmm in hmm_file:
             hmms.append(hmm)
+    logger.info(f"Loaded {len(hmms)} HMM profiles from '{hmm_db}'")
 
-    protein_id_to_hits: defaultdict[str, List[TaHit]] = defaultdict(list)
+    logger.info(f"Reading protein sequences from FASTA file: '{faa_file}'")
+    with pyhmmer.easel.SequenceFile(faa_file, digital=True) as seqs_file:
+        proteins = seqs_file.read_block()
 
-    for hits in pyhmmer.hmmsearch(hmms, proteins, E=e_value_threshold):
-        ta_name = hits.query.name.decode()
+        logger.info(f"Found {len(proteins)} protein sequences in '{faa_file}'")
 
-        for hit in hits:
-            protein_id = hit.name.decode()
-            protein_id_to_hits[protein_id].append(
-                TaHit(protein_id, ta_name, hit.score, hit.evalue)
-            )
+        logger.info(
+            f"Starting TA hit search with E-value threshold: {e_value_threshold}"
+        )
 
-    return protein_id_to_hits
+        protein_id_to_hits: defaultdict[str, List[TaHit]] = defaultdict(list)
+
+        for hits in track(
+            pyhmmer.hmmsearch(hmms, proteins, E=e_value_threshold),
+            total=len(proteins),
+            description="Searching for TA hits",
+        ):
+            ta_name = hits.query.name.decode()
+
+            for hit in hits:
+                protein_id = hit.name.decode()
+
+                protein_id_to_hits[protein_id].append(
+                    TaHit(protein_id, ta_name, hit.score, hit.evalue)
+                )
+
+        logger.info(f"Identified {len(protein_id_to_hits)} proteins with TA hits")
+        logger.info("TA hit search completed successfully")
+
+        return protein_id_to_hits
