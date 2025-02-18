b7fc9846af53453c472b99c485e72c28dbac33a2
JeanMainguy <jean.mainguy@outlook.fr>
2025-02-18 11:11:56 +0100

    update test models with change on contig attr

--- tests/models_test.py
+++ tests/models_test.py
@@ -6,7 +6,6 @@ def test_gene_initialization():
 
     assert gene.name == "gene1"
     assert gene.coordinates == [(1, 4)]
-    assert gene.contig is None
     assert gene.start == 1
     assert gene.stop == 4
 
@@ -16,7 +15,6 @@ def test_gene_initialization_with_join_coordinates():
 
     assert gene.name == "gene1"
     assert gene.coordinates == [(1, 4), (5, 8)]
-    assert gene.contig is None
     assert gene.start == 1
     assert gene.stop == 8
 
@@ -27,7 +25,6 @@ def test_cds_initialization():
     assert gene.name == "cds1"
     assert gene.protein_sequence == "MTDLA"
     assert gene.coordinates == [(1, 25)]
-    assert gene.contig is None
     assert gene.start == 1
     assert gene.stop == 25
 
@@ -41,5 +38,3 @@ def test_contig_initialization():
     assert len(contig.genes) == 2
     assert contig.genes[0].name == "gene1"
     assert contig.genes[1].name == "gene2"
-    assert gene1.contig == contig
-    assert gene2.contig == contig
